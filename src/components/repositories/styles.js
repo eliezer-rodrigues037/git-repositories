b8ee8429801eb01a281bea137942d3ea6b19512d
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2022-07-21 19:15:58 -0300

    Profile style 1.0

---
+++ src/components/repositories/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
+
+export const TabsContainer = styled(Tabs)``;
+
+export const TabListContainer = styled(TabList)``;
+
+export const TabContainer = styled(Tab)``;
+
+export const TabPanelContainer = styled(TabPanel)``;
